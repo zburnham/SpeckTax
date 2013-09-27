09f2fb3df502423e69b63ff9eea0ac396c4d4b19
Zachary Burnham <zburnham@gmail.com>
2013-09-27 14:09:08 -0400

    Changing composer.json back to point at the main speckcommerce repository Minor change to AbstractTaxEngine

--- src/SpeckTax/Engines/AbstractTaxEngine.php
+++ src/SpeckTax/Engines/AbstractTaxEngine.php
@@ -4,7 +4,16 @@ namespace SpeckTax\Engines;
 
 use SpeckTax\Interfaces\CalculateTaxInterface;
 
-abstract class AbstractTaxEngine implements CalculateTaxInterface
+class AbstractTaxEngine implements CalculateTaxInterface
 {
+    public function calculateTax()
+    {
+        ;
+    }
     
+    /**
+     * Should put commonly-used functions in here, for example
+     * code to load the rules from the database, determine what 
+     * tax engine to use, and so forth.
+     */
 }
